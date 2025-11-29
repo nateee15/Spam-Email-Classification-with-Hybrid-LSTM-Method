27723033a5d6812d507cc43bb396184a7c020028
Natasya Ega Lina Marbun <149855891+nateee15@users.noreply.github.com>
2025-11-30 05:03:35 +0700

    Update predict_pipeline.py

--- predict_pipeline.py
+++ predict_pipeline.py
@@ -1,13 +1,11 @@
 import pickle
 import numpy as np
+import onnxruntime as ort
 from tensorflow.keras.utils import pad_sequences
-from tensorflow.keras.models import load_model
 from preprocessing import preprocess_text
 from numeric_processing import extract_numeric_features
 
-# Load semua komponen
-model = load_model("spamnet_hybrid_attention.keras", compile=False)
-
+# Load tokenizer, scaler, label encoder
 tokenizer = pickle.load(open("tokenizer.pkl", "rb"))
 scaler     = pickle.load(open("scaler.pkl", "rb"))
 le_labels  = pickle.load(open("label_encoder.pkl", "rb"))
@@ -16,15 +14,30 @@ numeric_info = pickle.load(open("numeric_info.pkl", "rb"))
 maxlen           = numeric_info["maxlen"]
 numeric_features = numeric_info["numeric_features"]
 
-def predict_email(text, from_domain="unknown.com"):
-    processed = preprocess_text(text)
-    seq = tokenizer.texts_to_sequences([processed])
-    padded = pad_sequences(seq, maxlen=maxlen, padding='post')
+# Load ONNX model
+session = ort.InferenceSession("spamnet_hybrid_attention.onnx")
 
+def predict_email(text, from_domain="example.com"):
+    # Preprocess text
+    processed = preprocess_text(text)
+
+    # Tokenize → pad
+    seq = tokenizer.texts_to_sequences([processed])
+    padded = pad_sequences(seq, maxlen=maxlen, padding='post').astype(np.int32)
+
+    # Numerical features
     numeric_scaled = extract_numeric_features(
         text, from_domain, scaler, le_labels
-    )
+    ).astype(np.float32)
 
-    prob = model.predict([padded, numeric_scaled])[0][0]
+    # Run ONNX session
+    inputs = {
+        "text_input": padded,
+        "num_input": numeric_scaled
+    }
+    result = session.run(None, inputs)[0]
+    prob = float(result[0][0])
+
     label = "spam" if prob >= 0.5 else "ham"
-    return label, float(prob)
+
+    return label, prob
