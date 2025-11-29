df4c4d88b4d57091f3c93f843d5af782c57162e0
Natasya Ega Lina Marbun <149855891+nateee15@users.noreply.github.com>
2025-11-30 04:20:26 +0700

    Add files via upload

---
+++ predict_pipeline.py
@@ -0,0 +1,30 @@
+import pickle
+import numpy as np
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+from preprocessing import preprocess_text
+from numeric_processing import extract_numeric_features
+
+# Load semua komponen
+model = load_model("spamnet_hybrid_attention.keras", compile=False)
+
+tokenizer = pickle.load(open("tokenizer.pkl", "rb"))
+scaler     = pickle.load(open("scaler.pkl", "rb"))
+le_labels  = pickle.load(open("label_encoder.pkl", "rb"))
+numeric_info = pickle.load(open("numeric_info.pkl", "rb"))
+
+maxlen           = numeric_info["maxlen"]
+numeric_features = numeric_info["numeric_features"]
+
+def predict_email(text, from_domain="unknown.com"):
+    processed = preprocess_text(text)
+    seq = tokenizer.texts_to_sequences([processed])
+    padded = pad_sequences(seq, maxlen=maxlen, padding='post')
+
+    numeric_scaled = extract_numeric_features(
+        text, from_domain, scaler, le_labels
+    )
+
+    prob = model.predict([padded, numeric_scaled])[0][0]
+    label = "spam" if prob >= 0.5 else "ham"
+    return label, float(prob)
---
+++ preprocessing.py
@@ -0,0 +1,28 @@
+import re
+import numpy as np
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+import nltk
+from nltk.corpus import stopwords
+
+# load stopwords
+stop_words_indo = set(stopwords.words('indonesian'))
+stop_words_eng = set(stopwords.words('english'))
+stop_words = stop_words_indo.union(stop_words_eng)
+
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'http\S+', ' ', text)
+    text = re.sub(r'\d+', ' ', text)
+    text = re.sub(r'[^\w\s]', ' ', text)
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
+
+def preprocess_text(text):
+    text = clean_text(text)
+    tokens = text.split()
+    tokens = [w for w in tokens if w not in stop_words]
+    tokens = [stemmer.stem(w) for w in tokens]
+    return " ".join(tokens)
